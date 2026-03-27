61fc3e4eadb8d89c4c021143c7d0b8f3111c5650
mewennl <mwnl@sas.upenn.edu>
2026-03-27 02:40:54 -0400

    Created main file with flow of events, starting on methods

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+import asyncio
+import websockets
+import json
+import config
+import auth
+import time
+import math
+from openai import AsyncOpenAi
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+#set up openai client
+openai_client = AsyncOpenAi()
+
+REST_BASE = "https://api.gemini.com"
+
+class PredictionTrader:
+    """
+    Flow:
+    1. parse_intent()       -GPT extracts the certain event, # dollars, outcome from text input
+    2. search_market()      - REST finds associated market + contract
+    3. connect_account()    - Websocket connection
+    4. get_live_price()     - subscribe to book and listen for best ask
+    5. confirm_with_user()  - show cost and payout, confirm with user before placing
+    6. place_order()        - order.place over websocket
+    7. await_fill()         - listen to the orders@account endpoint
+
+    """
+
+    def connect_account(self):
+        pass
+
+    def place_order(self):
+        pass
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
